69ee68496ad7eec47f86c74358bd344e015f00e0
Flavio Carvalho <professorflaviosoares@gmail.com>
2024-08-17 12:49:11 -0300

    Disponibilizando o exercícios solicitado - acesso ao network bancend

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,37 +1,55 @@
-"""
-URL configuration for bookstore project.
+# """
+# URL configuration for bookstore project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
 
-# C:\Users\profe\Desktop\BOOKSTORE\bookstore\urls.py
+# # C:\Users\profe\Desktop\BOOKSTORE\bookstore\urls.py
 
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path, re_path, include
+# from rest_framework.authtoken.views import obtain_auth_token
+# import debug_toolbar  # Adicione esta linha para importar o debug_toolbar
+
+# from bookstore import views
+
+# urlpatterns = [
+#     path(
+#         "__debug__/", include(debug_toolbar.urls)
+#     ),  # Certifique-se de que esta linha está correta
+#     path("admin/", admin.site.urls),
+#     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
+#     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
+#     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+#     path("update_server/", views.update, name="update"),
+#     path("hello/", views.hello_world, name="hello_world"),
+# ]
+
+
 from django.urls import path, re_path, include
+from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 import debug_toolbar  # Adicione esta linha para importar o debug_toolbar
-
 from bookstore import views
 
 urlpatterns = [
-    path(
-        "__debug__/", include(debug_toolbar.urls)
-    ),  # Certifique-se de que esta linha está correta
+    path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello_world"),
+    path("", views.home, name="home"),  # Rota para a página inicial
 ]

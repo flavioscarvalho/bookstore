13be9f6751d3ed787495d35a7fdbe8d92cd67582
Flavio Carvalho <professorflaviosoares@gmail.com>
2024-10-29 22:00:15 -0300

    atualizando a branch e retornando a branch main para o deploy no HEROKU

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -1,130 +1,3 @@
-# import os
-# from pathlib import Path
-
-# # Diretório base do projeto
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# # Chave secreta para a aplicação Django
-# SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-_uql)*)q-k=yx15(*vw#ii-r7p_342(4viiznmi68+^cx=65tm")
-
-# # Modo de depuração (True para desenvolvimento, False para produção)
-# DEBUG = int(os.environ.get("DEBUG", default=1))
-
-# # Hosts permitidos para acessar a aplicação
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-
-# # Aplicações instaladas
-# INSTALLED_APPS = [
-#     "django.contrib.admin",
-#     "django.contrib.auth",
-#     "django.contrib.contenttypes",
-#     "django.contrib.sessions",
-#     "django.contrib.messages",
-#     "django.contrib.staticfiles",
-#     "django_extensions",
-#     "rest_framework",
-#     "order",
-#     "product",
-#     "debug_toolbar",
-#     "rest_framework.authtoken",
-# ]
-
-# # Middlewares configurados
-# MIDDLEWARE = [
-#     "django.middleware.security.SecurityMiddleware",
-#     "django.contrib.sessions.middleware.SessionMiddleware",
-#     "django.middleware.common.CommonMiddleware",
-#     "django.middleware.csrf.CsrfViewMiddleware",
-#     "django.contrib.auth.middleware.AuthenticationMiddleware",
-#     "django.contrib.messages.middleware.MessageMiddleware",
-#     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-#     "debug_toolbar.middleware.DebugToolbarMiddleware",
-# ]
-
-# # Configuração de URL raiz
-# ROOT_URLCONF = "bookstore.urls"
-
-# # Configuração dos templates
-# TEMPLATES = [
-#     {
-#         "BACKEND": "django.template.backends.django.DjangoTemplates",
-#         "DIRS": [],
-#         "APP_DIRS": True,
-#         "OPTIONS": {
-#             "context_processors": [
-#                 "django.template.context_processors.debug",
-#                 "django.template.context_processors.request",
-#                 "django.contrib.auth.context_processors.auth",
-#                 "django.contrib.messages.context_processors.messages",
-#             ],
-#         },
-#     },
-# ]
-
-# # Configuração WSGI
-# WSGI_APPLICATION = "bookstore.wsgi.application"
-
-# # Configuração do banco de dados
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#         "USER": os.environ.get("SQL_USER", "user"),
-#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#         "HOST": os.environ.get("SQL_HOST", "localhost"),
-#         "PORT": os.environ.get("SQL_PORT", "5432"),
-#     }
-# }
-
-# # Validações de senha
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-#     },
-#     {
-#         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-#     },
-# ]
-
-# # Configurações de internacionalização
-# LANGUAGE_CODE = "pt-br"
-# TIME_ZONE = "America/Sao_Paulo"
-# USE_I18N = True
-# USE_L10N = True
-# USE_TZ = True
-
-# # Configuração dos arquivos estáticos
-# STATIC_URL = "static/"
-
-# # Configuração do campo de chave primária padrão
-# DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# # IPs internos para ferramentas de depuração
-# INTERNAL_IPS = [
-#     "127.0.0.1",
-# ]
-
-# # Configurações do Django REST Framework, incluindo paginação
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10,  # Número de itens por página
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.TokenAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#     ],
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ],
-# }
-
-
-
 import os
 from pathlib import Path
 
@@ -138,7 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-_uql)*)q-k=yx15(*vw#i
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
 # Hosts permitidos para acessar a aplicação
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ebac-api-bookstore-469dc7be49a5.herokuapp.com']
 
 # Aplicações instaladas
 INSTALLED_APPS = [
@@ -159,6 +32,7 @@ INSTALLED_APPS = [
 # Middlewares configurados
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Corrigido: "whitnoise" para "whitenoise"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -167,6 +41,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
 
 # Configuração de URL raiz
 ROOT_URLCONF = "bookstore.urls"
@@ -190,6 +65,10 @@ TEMPLATES = [
 
 # Configuração WSGI
 WSGI_APPLICATION = "bookstore.wsgi.application"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configuração do banco de dados
 DATABASES = {
